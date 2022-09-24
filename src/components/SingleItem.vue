9e7c1646851323ad023629c581837d05aafa271a
r1d3rzz <myintthwaykhine127@gmail.com>
2022-09-24 20:39:05 +0630

    fetch api and filter items with search bar

---
+++ src/components/SingleItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="itemContainer">
+    <div style="text-align: start">
+      {{ item.title }}
+      <p>Price : ${{ item.price }}.00</p>
+      <p class="viewMore">View More</p>
+    </div>
+    <div>
+      <img :src="item.category.image" width="100" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style scoped>
+.itemContainer {
+  background-color: rgb(36, 28, 28);
+  color: white;
+  padding: 20px 10px;
+  width: 400px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.viewMore {
+  cursor: pointer;
+  user-select: none;
+  color: yellow;
+  transition: 0.2s ease;
+}
+.viewMore:hover {
+  color: rgba(246, 255, 0, 0.754);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input
+      type="text"
+      placeholder="Search ...."
+      v-model="search"
+      class="searchBox"
+    />
+  </div>
+
+  <div v-if="items.length > 0">
+    <div v-for="item in searchItems" :key="item.id">
+      <SingleItem :item="item" />
+    </div>
+  </div>
+
+  <div v-else>
+    <div>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleItem from "../components/SingleItem";
+import { ref } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: { SingleItem },
+  setup() {
+    let api = "https://api.escuelajs.co/api/v1/products";
+
+    let items = ref([]);
+    let search = ref("");
+
+    let getItems = async () => {
+      try {
+        let res = await fetch(api);
+        if (res.status === 404) {
+          throw new Error("url not found");
+        }
+        let data = await res.json();
+        items.value = data;
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
+    let searchItems = computed(() => {
+      return items.value.filter((item) => {
+        return item.title.toLowerCase().includes(search.value.toLowerCase());
+      });
+    });
+
+    getItems();
+
+    return { items, search, searchItems };
+  },
+};
 </script>
+
+<style>
+.searchBox {
+  width: 400px;
+  margin: 20px 0;
+  padding: 10px 5px;
+  border-radius: 5px;
+  font-size: 16px;
+}
+</style>
